407e5c381be7b08506d00830847c7dcde49af426
Sumeyra <smyrbdr@gmail.com>
2018-02-26 16:25:36 +0300

    Update duyurubul.py

--- duyurubul.py
+++ duyurubul.py
@@ -1,6 +1,5 @@
-# Note - this code must run in Python 2.x and you must download
-# http://www.pythonlearn.com/code/BeautifulSoup.py
-# Into the same folder as this program
+# Note - this code must run in Python 2.x and you must download BeautifulSoup.py
+# into the same folder as this program
 
 import urllib
 from BeautifulSoup import *
@@ -25,10 +24,10 @@ for tag in tags:
         str2 = urllink  
         break
 
-
 text = "Duyuru Basligi: " + str1 + " " + "Duyuru Linki: " + str2
 print text, len(text)
 
+#Fill in appropriate OAuth keys
 api = twitter.Api(consumer_key='', consumer_secret='', access_token_key='', access_token_secret='')
 
 status = api.PostUpdate(str1 + " >> " + str2)
